5dbf498848036dac7b96553df3943ed1c8e7309d
Márcio da Mota Xerém <mxerem@icloud.com>
2025-01-16 16:07:38 -0300

    terceiro commit - Adição do módulo com o carrinho ao layout principal

--- marcio-xerem/script.js
+++ marcio-xerem/script.js
@@ -1,38 +1,56 @@
-const _$ = vid => document.getElementById(vid) || document.querySelector(vid);
-
 const init = () => {
     const produtos = [
-        {imagem: './assets/images/tenis1.jpg', nome: 'Produto 1', descricao: 'Descrição do Produto 1', preco: 150.00},
-        {imagem: './assets/images/tenis2.jpg', nome: 'Produto 2', descricao: 'Descrição do Produto 2', preco: 200.00},
-        {imagem: './assets/images/roupa1.jpeg', nome: 'Produto 3', descricao: 'Descrição do Produto 3', preco: 250.00},
-        {imagem: './assets/images/roupa2.png', nome: 'Produto 4', descricao: 'Descrição do Produto 4', preco: 250.00},
-        {imagem: './assets/images/roupa3.png', nome: 'Produto 5', descricao: 'Descrição do Produto 5', preco: 90.00},
-        {imagem: './assets/images/tenis3.png', nome: 'Produto 6', descricao: 'Descrição do Produto 6', preco: 230.00}
+        {imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/nike.png', marca: 'Nike', nome: 'Nike Dunk 3 Panda', preco: 299.99},
+        {imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/nike2.png', marca: 'Nike', nome: 'Jaqueta Windrunner Masculina', preco: 579.99},
+        {imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/adidas2.png', marca: 'Adidas', nome: 'Chuteira X SpeedFlow Campo', preco: 439.99}
     ];
 
     const carrinho = [];
+    const containerProdutos = document.querySelector('.container-produtos');
+    const carrinhoDiv = document.querySelector('.lista-produtos');
 
-    produtos.forEach((produto, index) => {
+    produtos.forEach(produto => {
         const card = criarCard(produto);
-        const coluna = _$(`coluna${(index % 3) + 1}`);
-        coluna.appendChild(card);
+        containerProdutos.appendChild(card);
     });
 
     function criarCard(produto) {
         const card = document.createElement('div');
-        card.className = 'card';
+        card.className = 'card-produto col-4';
+
         card.innerHTML = `
-            <img src="${produto.imagem}" class="card-img-top" alt="${produto.nome}">
-            <div class="card-body">
-                <h5 class="card-title">${produto.nome}</h5>
-                <p class="card-text">${produto.descricao}</p>
-                <p class="card-text">Preço: R$ ${produto.preco.toFixed(2)}</p>
-                <button class="btn btn-primary">Adicionar ao Carrinho</button>
+            <div class="produto">
+                <img src="${produto.imagem}">
+                <button class="btn-add"> <img src="assets/icon-add-to-cart.svg" alt="">Add to cart</button>
+
+                <button class="btn-controller" type="button">
+                    <div class="controller"><img src="assets/icon-decrement-quantity.svg"></div>
+                    <span class="quantidade">1</span>
+                    <div class="controller"><img src="assets/icon-increment-quantity.svg"></div>
+                </button>
+            </div>
+            <div class="infos">
+                <span class="marca">${produto.marca}</span>
+                <span class="nome">${produto.nome}</span>
+                <span class="preco">R$ ${produto.preco.toFixed(2)}</span>
             </div>
         `;
 
-        card.querySelector('button').addEventListener('click', () => {
+        card.querySelector('.btn-add').addEventListener('click', () => {
             adicionarAoCarrinho(produto);
+        });
+
+        card.querySelectorAll('.controller img').forEach((btn, index) => {
+            btn.addEventListener('click', () => {
+                const quantidadeElement = card.querySelector('.quantidade');
+                let quantidade = parseInt(quantidadeElement.innerText);
+                if (index === 0 && quantidade > 1) {
+                    quantidade--;
+                } else if (index === 1) {
+                    quantidade++;
+                }
+                quantidadeElement.innerText = quantidade;
+            });
         });
 
         return card;
@@ -49,14 +67,15 @@ const init = () => {
     }
 
     function atualizarCarrinho() {
-        const carrinhoDiv = _$('carrinho');
-        carrinhoDiv.innerHTML = '<h4>Carrinho de Compras</h4>';
+        carrinhoDiv.innerHTML = '';
         let total = 0;
 
         carrinho.forEach(produto => {
             const totalItem = produto.quantidade * produto.preco;
             carrinhoDiv.innerHTML += `
-                <p>${produto.descricao} - Quantidade: ${produto.quantidade} - Valor Unitário: R$ ${produto.preco.toFixed(2)} - Total: R$ ${totalItem.toFixed(2)}</p>
+                <div class="produto-carrinho">
+                    <span>${produto.nome} - Quantidade: ${produto.quantidade} - Total: R$ ${totalItem.toFixed(2)}</span>
+                </div>
             `;
             total += totalItem;
         });
