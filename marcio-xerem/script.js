f493e197a4e9ca0de9f58565d9a8fd8351f298a5
Márcio da Mota Xerém <mxerem@icloud.com>
2025-01-18 10:54:28 -0300

    sexto commit - Inclusão do modal resumo da compra ao programa principal

--- marcio-xerem/script.js
+++ marcio-xerem/script.js
@@ -1,16 +1,29 @@
+import Carrinho from "./Carrinho.js";
+import Produto from "./Produto.js";
+
 const _$ = vid => document.getElementById(vid) || document.querySelector(vid);
 
-const init = () => {
-    const produtos = [
-        {imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/nike.png', marca: 'Nike', nome: 'Nike Dunk 3 Panda', preco: 299.99},
-        {imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/nike2.png', marca: 'Nike', nome: 'Jaqueta Windrunner Masculina', preco: 579.99},
-        {imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/adidas2.png', marca: 'Adidas', nome: 'Chuteira X SpeedFlow Campo', preco: 439.99}
-    ];
+const dados = [
+    { id: 1, imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/nike.png', marca: 'Nike', nome: 'Nike Dunk 3 Panda', preco: 299.99, descricao: "Descrição do prod" },
+    { id: 2, imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/nike2.png', marca: 'Nike', nome: 'Jaqueta Windrunner Masculina', preco: 579.99, descricao: "Descrição prod" },
+    { id: 3, imagem: 'https://nossaempresa.netlify.app/loja/assets/produtos/adidas2.png', marca: 'Adidas', nome: 'Chuteira X SpeedFlow Campo', preco: 439.99, descricao: "Descrição prod" }
+];
 
-    const carrinho = [];
+const produtos = new Array();
+
+dados.forEach(produto => {
+    const produtoObject = new Produto(produto.id, produto.nome, produto.marca, produto.descricao, produto.preco, produto.imagem);
+    produtos.push(produtoObject);
+});
+console.log(produtos);
+const carrinho = new Carrinho();
+
+const init = () => {
     const containerProdutos = _$('.container-produtos');
     const carrinhoDiv = _$('.lista-produtos');
-
+    const totalCarrinho = _$('.carrinho > h2');
+    const valorTotal = _$('.valor-total-carrinho');
+    
     produtos.forEach(produto => {
         const card = criarCard(produto);
         containerProdutos.appendChild(card);
@@ -19,17 +32,18 @@ const init = () => {
     function criarCard(produto) {
         const card = document.createElement('div');
         card.className = 'card-produto col-4';
+        card.dataset.id = produto.id;
 
         card.innerHTML = `
             <div class="produto">
                 <img src="${produto.imagem}">
                 <button class="btn-add"> <img src="assets/icon-add-to-cart.svg" alt="">Add to cart</button>
 
-                <button class="btn-controller" type="button">
-                    <div class="controller"><img src="assets/icon-decrement-quantity.svg"></div>
+                <div class="btn-controller" style="display: none;">
+                    <div class="controller decrement"><img src="assets/icon-decrement-quantity.svg"></div>
                     <span class="quantidade">1</span>
-                    <div class="controller"><img src="assets/icon-increment-quantity.svg"></div>
-                </button>
+                    <div class="controller increment"><img src="assets/icon-increment-quantity.svg"></div>
+                </div>
             </div>
             <div class="infos">
                 <span class="marca">${produto.marca}</span>
@@ -38,57 +52,73 @@ const init = () => {
             </div>
         `;
 
-        card.querySelector('.btn-add').addEventListener('click', () => {
-            adicionarAoCarrinho(produto);
+        const btnAddToCart = card.querySelector('.btn-add');
+        const btnController = card.querySelector('.btn-controller');
+        const quantidadeElement = card.querySelector('.quantidade');
+        const btnDecrement = card.querySelector('.decrement');
+        const btnIncrement = card.querySelector('.increment');
+
+        btnAddToCart.addEventListener('click', () => {
+            btnController.style.display = 'flex';
+            btnAddToCart.style.display = 'none';
+            carrinho.addProduto(produto);
+            quantidadeElement.textContent = produto.quantidade;
+            atualizarCarrinho();
         });
 
-        card.querySelectorAll('.controller img').forEach((btn, index) => {
-            btn.addEventListener('click', () => {
-                const quantidadeElement = card.querySelector('.quantidade');
-                let quantidade = parseInt(quantidadeElement.innerText);
-                if (index === 0 && quantidade > 1) {
-                    quantidade--;
-                } else if (index === 1) {
-                    quantidade++;
-                }
-                quantidadeElement.innerText = quantidade;
-            });
+        btnDecrement.addEventListener('click', () => {
+            produto.decrementar();
+            if (produto.quantidade > 0) {
+                quantidadeElement.textContent = produto.quantidade;
+            } else {
+                btnController.style.display = 'none';
+                btnAddToCart.style.display = 'flex';
+                carrinho.removerProduto(produto);
+            }
+            atualizarCarrinho();
+        });
+
+        btnIncrement.addEventListener('click', () => {
+            produto.incrementar();
+            quantidadeElement.textContent = produto.quantidade;
+            atualizarCarrinho();
         });
 
         return card;
     }
 
-    function adicionarAoCarrinho(produto) {
-        const item = carrinho.find(item => item.nome === produto.nome);
-        if (item) {
-            item.quantidade++;
-        } else {
-            carrinho.push({ ...produto, quantidade: 1 });
-        }
-        atualizarCarrinho();
-    }
-
     function atualizarCarrinho() {
         carrinhoDiv.innerHTML = '';
-        let total = 0;
 
-        carrinho.forEach(produto => {
+        carrinho.listaProdutos.forEach(produto => {
             const totalItem = produto.quantidade * produto.preco;
             carrinhoDiv.innerHTML += `
                 <div class="produto-carrinho">
                     <span>${produto.nome} - Quantidade: ${produto.quantidade} - Total: R$ ${totalItem.toFixed(2)}</span>
                 </div>
             `;
-            total += totalItem;
         });
 
-        carrinhoDiv.innerHTML += `
-            <h5>Valor Total: R$ ${total.toFixed(2)}</h5>
-            <button class="btn btn-success btn-confirmar">Confirmar Pedido</button>
-        `;
+        valorTotal.innerHTML = `<h5>Valor Total: R$ ${carrinho.getValorTotal().toFixed(2)}</h5>`;
+
+        totalCarrinho.textContent = `Your Cart (${carrinho.getQuantidadeProdutos()})`;
 
         const btnConfirmar = _$('.btn-confirmar');
-        btnConfirmar.addEventListener('click', confirmarPedido);
+        if (carrinho.getValorTotal() > 0) {
+            if (!btnConfirmar) {
+                const confirmarBtn = document.createElement('button');
+                confirmarBtn.className = 'btn btn-success btn-confirmar';
+                confirmarBtn.textContent = 'Confirmar Pedido';
+                confirmarBtn.addEventListener('click', confirmarPedido);
+                carrinhoDiv.appendChild(confirmarBtn);
+            }
+        } else if (btnConfirmar) {
+            btnConfirmar.remove();
+        }
+        
+        if (carrinho.getValorTotal() == 0) {
+            carrinhoDiv.innerHTML = `<img src="assets/carrinho.png" class="vazio">`;
+        }
     }
 
     function confirmarPedido() {
@@ -96,10 +126,10 @@ const init = () => {
         modal.className = 'modal';
         modal.style.display = 'flex';
 
-        let total = 0; //Acumulador para o total das compras
+        let total = 0;
         let produtosHTML = '';
 
-        carrinho.forEach(produto => {
+        carrinho.listaProdutos.forEach(produto => {
             const totalItem = produto.quantidade * produto.preco;
             produtosHTML += `
                 <div class="produto-confirmacao">
@@ -115,11 +145,11 @@ const init = () => {
 
         modal.innerHTML = `
             <div class="modal-content">
-                <h2>Order Confirmed</h2>
-                <p>We hope you enjoy your food!</p>
+                <h2>Pedido Confirmado</h2>
+                <p>Agradecemos por comprar conosco!</p>
                 ${produtosHTML}
                 <h5>Valor Total do Pedido: R$ ${total.toFixed(2)}</h5>
-                <button class="btn btn-novo-pedido">Start New Order</button>
+                <button class="btn btn-novo-pedido">Iniciar Novo Pedido</button>
             </div>
         `;
 
@@ -127,11 +157,10 @@ const init = () => {
 
         _$('.btn-novo-pedido').addEventListener('click', () => {
             modal.remove();
-            carrinho.length = 0; // Limpar o carrinho
-            atualizarCarrinho(); // Atualizar o carrinho visualmente
+            carrinho.limpar();
+            atualizarCarrinho();
         });
 
-        // Adiciona a classe para congelar o fundo
         document.body.classList.add('modal-open');
     }
 };
